b7a580f2a74c152b853731eac0f19a3026c9844a
Brenden Jones <brendenjones4320@gmail.com>
2017-06-28 11:57:52 -0400

    create a flick objct with name and ID

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = {
+    init(formSelector){
+        this.max = 0
+        document
+        .querySelector(formSelector)
+        .addEventListener('submit', this.handleSubmit.bind(this))
+    },
+    handleSubmit(ev){
+        ev.preventDefault()
+        const flick= {
+            id: this.max + 1,
+            name: f.flickName.value,
+        }
+        console.log(flick)
+        this.max++
+    },
+}
+
+app.init('form#flick-form')
